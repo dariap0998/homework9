16aa0b5aab3549b384d6f8c5e519ddfb6f64ae57
darcyp <eelakoopfm@gmail.com>
2023-02-09 23:41:56 +0200

    task 2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     }
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -17,7 +18,26 @@ public class Main {
             int sum  = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i]; }
-            System.out.println(sum);
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int min = 201000;
+        int max = 99999;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+            System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей");
     }
 }
+
+
 
